244ac361a2973a8240c585232fded0438cfe9b43
keran.marinov <linxmap@gmail.com>
2018-06-16 13:23:39 +0100

    added product details, media, additional services to product resource and tests

--- ShoppingApp/Product/resource/ProductResource.swift
+++ ShoppingApp/Product/resource/ProductResource.swift
@@ -7,19 +7,41 @@ struct ProductResource: Resource, HttpSchemeBuilder {
     private(set) var imageUrl: String?
     private(set) var displaySpecialOffer: String
     private(set) var code: String
-    private let priceResource: PriceResource
+    private let price: PriceResource
+    private let details: ProductDetailsResource
+    private let additionalServices: ProductAdditionalServicesResource
+    private let media: ProductMediaResource
 
     init(json: JSON) {
         id = json["productId"].intValue
         title = json["title"].stringValue
-        priceResource = PriceResource(json: json["price"])
         displaySpecialOffer = json["displaySpecialOffer"].stringValue
         code = json["code"].stringValue
+        price = PriceResource(json: json["price"])
+        details = ProductDetailsResource(json: json["details"])
+        additionalServices = ProductAdditionalServicesResource(json: json["additionalServices"])
+        media = ProductMediaResource(json: json["media"])
         imageUrl = addHttpScheme(to: json["image"].stringValue)
     }
 
     func priceNowFormatted() -> String {
-        return priceResource.priceNowFormatted()
+        return price.priceNowFormatted()
+    }
+
+    func information() -> String {
+        return details.information
+    }
+
+    func includedServicesFormatted() -> String {
+        return additionalServices.includedServicesFormatted()
+    }
+
+    func features() -> [ProductFeatureResource] {
+        return details.features
+    }
+
+    func imageUrls() -> [String] {
+        return media.imageUrls
     }
 
 }
--- ShoppingAppTests/Product/resource/ProductResourceTests.swift
+++ ShoppingAppTests/Product/resource/ProductResourceTests.swift
@@ -15,6 +15,10 @@ class ProductResourceTests: XCTestCase {
         XCTAssertNil(product.imageUrl)
         XCTAssertEqual(product.code, "")
         XCTAssertEqual(product.priceNowFormatted(), "")
+        XCTAssertEqual(product.information(), "")
+        XCTAssertEqual(product.includedServicesFormatted(), "")
+        XCTAssertTrue(product.features().isEmpty)
+        XCTAssertTrue(product.imageUrls().isEmpty)
     }
 
     func testInitialize_whenHasValidProductJson_returnsMappedValues() {
@@ -27,6 +31,10 @@ class ProductResourceTests: XCTestCase {
         XCTAssertEqual(product.displaySpecialOffer, "buy 1 free get 1 free")
         XCTAssertEqual(product.code, "81701107")
         XCTAssertEqual(product.priceNowFormatted(), "£349.00")
+        XCTAssertEqual(product.information(), "<p>Reliable and versatile, the Freestanding Dishwasher from Bosch</p>")
+        XCTAssertEqual(product.includedServicesFormatted(), "2 year guarantee included, 1 month free service")
+        XCTAssertEqual(product.features().count, 4)
+        XCTAssertEqual(product.imageUrls().count, 4)
     }
 
 }
